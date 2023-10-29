f006a2c7ff97276be94192f79fc6ce537f9f6ef2
ibrohim5009 <ibrohimabdurayimov5@gmail.com>
2023-10-29 12:10:01 +0300

    efwwefw

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
+import About from "./pages/about/About";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <About/>
+  </div>;
 }
 
 export default App;
