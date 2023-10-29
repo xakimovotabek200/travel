8e4466dfbfc2e525c5260ad5f50fff48c45e7dca
ibrohim5009 <ibrohimabdurayimov5@gmail.com>
2023-10-29 19:00:27 +0300

    wefwfw

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
-// import TravelExhibition from "./components/sodiqjon_exhibition/header_sodiqjon";
+import TravelExhibition from "./components/sodiqjon_exhibition/header_sodiqjon";
 import Landscapes from "./pages/Landscapes/Landscapes";
-// import About from "./pages/about/About";
+import About from "./pages/about/About";
 
 function App() {
   return (
