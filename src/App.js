30f7bc9739207372a5c4716ad98aa6b9e0387b01
Your Name <you@example.com>
2023-10-29 15:28:27 +0500

    shaftoli bormi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
-import Landscapes from "./pages/Landscapes/Landscapes";
+import React from 'react';
+import {Routes, Route} from 'react-router-dom'
+import './App.css'
+import Landscapes from './pages/Landscapes/Landscapes'
+import TravelExhibition from './components/travel_exhibition/header_sodiqjon'
+// import Home from './pages/Landscapes/Landscapes'
+
 function App() {
-  return <Landscapes />;
+  return (
+    <div>
+      <Landscapes />
+      <TravelExhibition />
+        {/* <Routes> */}
+          {/* <Route path="/Home" element={<Home />} /> */}
+        {/* </Routes> */}
+    </div>
+  );
 }
 
 export default App;
--- src/pages/Landscapes/Landscapes.jsx
+++ src/pages/Landscapes/Landscapes.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Landscapes() {
   return (
-    <div>Landscapes</div>
+    <div></div>
   )
 }
 
