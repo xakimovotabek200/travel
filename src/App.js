909032df7194d6ba2e3c29bf100547485bb506b7
Your Name <you@example.com>
2023-10-29 16:41:50 +0500

    1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import TravelExhibition from "./components/travel_exhibition/header_sodiqjon";
+import TravelExhibition from "./components/sodiqjon_exhibition/header_sodiqjon";
 import Landscapes from "./pages/Landscapes/Landscapes";
 import About from "./pages/about/About";
 
---
+++ src/components/sodiqjon_exhibition/img_text.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function img_text() {
+  return (
+    <div className='img_text'>
+        <div className="img"></div>
+        <div className="text">    </div>        
+    </div>
+  )
+}
+
+export default img_text
