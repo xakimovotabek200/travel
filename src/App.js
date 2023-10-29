c1835223d74a714a603ddf27067ca99fb3974168
abduxoshimgafurov <abduxoshimgofurov79@gmail.com>
2023-10-29 20:55:42 +0500

    fgfqwfwqvw

--- src/App.js
+++ src/App.js
@@ -2,24 +2,14 @@ import React from "react";
 import "./App.css";
 // import TravelExhibition from "./components/sodiqjon_exhibition/header_sodiqjon";
 import Landscapes from "./pages/Landscapes/Landscapes";
-<<<<<<< HEAD
-import About from "./pages/about/About";
-function App() {
-  return <>
-    <About />
-
-    {/* <Landscapes />; */}
-  </>
-=======
 // import About from "./pages/about/About";
->>>>>>> 1fa9bf29e77e1e0913152a8694f897ca65cf5308
 
 function App() {
   return (
     <div>
       <Landscapes />
-      {/* <TravelExhibition />
-      <About /> */}
+      <TravelExhibition />
+      <About />
     </div>
   );
 }
