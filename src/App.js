bb2b6411589047f392d49b5a36f1bb7111056c15
xakimovotabek200 <xakimovoatbek@gmail.com>
2023-10-31 20:22:28 +0500

    add boldi

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import React from "react";
+  import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
-<<<<<<< HEAD
-import TravelExhibition from "./components/sodiqjon_exhibition/header_sodiqjon";
-import Landscapes from "./pages/Landscapes/Landscapes";
-// import About from "./pages/about/About";
-import { Index } from "./pages/itravel/end";
-import Home from "./Home/home.jsx"
-=======
-import Landscapes from "./pages/Landscapes/Landscapes";
->>>>>>> 71634bc0a7e4d6b0d3f3934f1fc5985cb4c7532a
+import Home from "./Home/home";
 
 function App() {
   return (
-    <div>
-<<<<<<< HEAD
-      <Home />  
-      <Index />
-      <Landscapes />
-      <TravelExhibition />
-      {/* <About /> */}
-      {/* <About /> */}
-=======
-      <Landscapes />
->>>>>>> 71634bc0a7e4d6b0d3f3934f1fc5985cb4c7532a
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import Home from './Home/home'
-import Navbar from './Home/navbar'
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
->>>>>>> 71634bc0a7e4d6b0d3f3934f1fc5985cb4c7532a
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
