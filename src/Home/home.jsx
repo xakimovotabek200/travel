d57b949211db52e3c2f825fcebc85692835a7f31
Your Name <you@example.com>
2023-10-31 20:06:54 +0500

    jndc

--- src/Home/home.jsx
+++ src/Home/home.jsx
@@ -1,226 +1,93 @@
-import React from "react";
-import "./home.css";
-import flyemirates from "./img/image 909.png";
-import guide1 from "./img/tour-guide 1.png";
-import airbnb from "./img/image 911.png";
-import turkish from "./img/image 913.png";
-import aswiss from "./img/image 912 (1).png";
-import romanticOpa from "./img/image 50.png";
-import banner1 from "./img/Rectangle 19294.png";
-import kichikrasm1 from "./img/Rectangle 19296.png";
-import paris from "./img/image 51.png";
-import plane from "./img/travelling 1.png";
-import hands1 from "./img/hands 1.png";
-import medicalteam1 from "./img/medical-team 1.png";
-import group7 from "./img/Group 7.svg";
-import planeImage from "./img/image 872.png";
-import hawaii from "./img/Rectangle 17 (1).png";
-import product1 from "./img/Rectangle 19305.png";
-import logo1 from "./img/Ellipse 623.svg";
-import gameOver from "./gameOver";
-import "./home.css";
-// import logo1 from "./img/Ellipse 623.svg";
-// import group7 from "./img/Group 7.svg";
-// import hawaii from "./img/Rectangle 17 (1).png";
-// import kichikrasm1 from "./img/Rectangle 19296.png";
-// import product1 from "./img/Rectangle 19305.png";
-// import hands1 from "./img/hands 1.png";
-// import romanticOpa from "./img/image 50.png";
-// import paris from "./img/image 51.png";
-// import planeImage from "./img/image 872.png";
-// import flyemirates from "./img/image 909.png";
-// import medicalteam1 from "./img/medical-team 1.png";
-// import guide1 from "./img/tour-guide 1.png";
-// import plane from "./img/travelling 1.png";
+import React from 'react';
+import './home.css';
 
-import person from "./img/ooui_user-avatar-outline.svg";
-import HomeRoutes from "../components/HomeRouters/HomeRoutes";
+import image909 from '../';
+import tourGuide from './img/tour-guide1.png';
+import image911 from './img/image911.png';
+import image913 from './img/image913.png';
+import image912 from './img/image912(1).png';
+import image50 from './img/image50.png';
+import rectangle19294 from './img/Rectangle19294.png';
+import rectangle19296 from './img/Rectangle19296.png';
+import image51 from './img/image51.png';
+import travelling1 from './img/travelling1.png';
+import hands1 from './img/hands1.png';
+import medicalTeam1 from './img/medical-team1.png';
+import group7 from './img/Group7.svg';
+import image872 from './img/image872.png';
+import rectangle17 from './img/Rectangle17(1).png';
+import rectangle19305 from './img/Rectangle19305.png';
+import ellipse623 from './img/Ellipse623.svg';
+import userAvatar from './img/ooui_user-avatar-outline.svg';
 
-function Home() {
+const Home = () => {
   return (
     <div className="allofthem">
       <div className="landing">
-        <HomeRoutes />
-        <div className="main_text">
-          <h1>
-            No matter where <br /> you’re going to, we’ll <br /> take you there
-          </h1>
-          <div className="request">
-            <input type="text" placeholder="Where to" />
-            <h2>Travel type</h2>
-            <h2>duration</h2>
-            <button>Submit</button>
-          </div>
+        <div className="container">
+          <h1>Welcome to our Travel Agency</h1>
+          <h2>Explore the world with us</h2>
         </div>
       </div>
-      {/* Clients */}
+
       <div className="clients">
-        <img src={flyemirates} alt="" />
-        <img src={flyemirates} alt="" />
-        <img src={flyemirates} alt="" />
-        <img src={flyemirates} alt="" />
-        <img src={flyemirates} alt="" />
-      </div>
-      <div classname="services">
-        <h5 className="small_red">CATEGORY</h5>
-        <h1>We Offer Best Services</h1>
-        <div className="cards">
-          <div className="card">
-            <img src={guide1} alt="" />
-            <h1>Guided tours</h1>
-            <h3>
-              sunt qui repellat saepe quo velit aperiam id aliquam placeat.
-            </h3>
-          </div>
-          <div className="card">
-            <img src={plane} alt="" />
-            <h1>Guided tours</h1>
-            <h3>
-              sunt qui repellat saepe quo velit aperiam id aliquam placeat.
-            </h3>
-          </div>
-          <div className="card">
-            <img src={hands1} alt="" />
-            <h1>Guided tours</h1>
-            <h3>
-              sunt qui repellat saepe quo velit aperiam id aliquam placeat.
-            </h3>
-          </div>
-          <div className="card">
-            <img src={medicalteam1} alt="" />
-            <h1>Guided tours</h1>
-            <h3>
-              sunt qui repellat saepe quo velit aperiam id aliquam placeat.
-            </h3>
-          </div>
+        <div className="container">
+          <h2>Our Clients</h2>
+          <img src={image909} alt="Client" />
+          <img src={tourGuide} alt="Client" />
+          <img src={image911} alt="Client" />
+          <img src={image913} alt="Client" />
         </div>
       </div>
-      {/* romantic section */}
+
+      <div className="services">
+        <div className="container">
+          <h2>Our Services</h2>
+          <img src={image912} alt="Service" />
+          <img src={image50} alt="Service" />
+          <img src={rectangle19294} alt="Service" />
+          <img src={rectangle19296} alt="Service" />
+        </div>
+      </div>
 
       <div className="romantic">
-        <div className="romantic_pictures">
-          <img src={romanticOpa} alt="" />
-        </div>
-        <div className="romantic_text">
-          <h5 className="small_red">Honeymoon Specials</h5>
-          <h1>
-            Our Romantic Tropical <br /> Destinations
-          </h1>
-          <h4>
-            Et labore harum non nobis ipsum eum molestias mollitia et corporis
-            praesentium a laudantium internos. Non quis eius quo eligendi
-            corrupti et fugiat nulla qui soluta recusandae in maxime quasi aut
-            ducimus illum aut optio quibusdam!
-          </h4>
-          <button className="redbutton">View packages</button>
+        <div className="container">
+          <h2>Experience Romantic Getaways</h2>
+          <img src={image51} alt="Romantic Getaway" />
         </div>
       </div>
-      {/* fast & easy */}
 
       <div className="fast-easy">
-        <div className="fast_left">
-          <h5 className="small_red">Fast & easy</h5>
-          <h1>
-            Get Your Favourite <br /> Resort Bookings
-          </h1>
-          <div className="tips">
-            <div className="tip1">
-              <img src={group7} alt="" />
-              <div className="tip1text">
-                <h1>Choose destianation</h1>
-                <h5>Lorem ipsum dolor sit amet, consectetur </h5>
-              </div>
-            </div>
-            <div className="tip1">
-              <img src={group7} alt="" />
-              <div className="tip1text">
-                <h1>Choose destianation</h1>
-                <h5>Lorem ipsum dolor sit amet, consectetur </h5>
-              </div>
-            </div>
-            <div className="tip1">
-              <img src={group7} alt="" />
-              <div className="tip1text">
-                <h1>Choose destianation</h1>
-                <h5>Lorem ipsum dolor sit amet, consectetur </h5>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="fast_right">
-          <img src={planeImage} alt="" />
-          <div className="complicated_cart">
-            <img src={hawaii} alt="" />
-            <h1>Trip to Hawaii</h1>
-            <h5>14-29 June | by JR Martir</h5>
-            <h6>60 people are interested</h6>
-          </div>
+        <div className="container">
+          <h2>Fast & Easy Booking</h2>
+          <img src={travelling1} alt="Fast & Easy Booking" />
+          <img src={hands1} alt="Fast & Easy Booking" />
         </div>
       </div>
-      <div className="banner1">
-        <h1>
-          Let’s make your <br /> next holiday amazing
-        </h1>
-      </div>
-      <div className="promotion">
-        <div className="promotion_left">
-          <h5 className="small_red">PROMOTION</h5>
-          <h1>
-            We Provide You Best <br /> Europe Sightseeing Tours
-          </h1>
-          <h3>
-            Et labore harum non nobis ipsum eum molestias mollitia et corporis
-            praesentium a laudantium internos. Non quis eius quo eligendi
-            corrupti et fugiat nulla qui soluta recusandae in maxime quasi aut
-            ducimus illum aut optio quibusdam!
-          </h3>
-          <button className="redbutton">View packages</button>
-          <div className="small_gallery">
-            <div className="rasmcard">
-              <h4>$700</h4>
-              <img src={kichikrasm1} alt="" />
-            </div>
 
-            <div className="rasmcard">
-              <h4>$700</h4>
-              <img src={kichikrasm1} alt="" />
-            </div>
-            <div className="rasmcard">
-              <h4>$700</h4>
-              <img src={kichikrasm1} alt="" />
-            </div>
-            <div className="rasmcard">
-              <h4>$700</h4>
-              <img src={kichikrasm1} alt="" />
-            </div>
-          </div>
-        </div>
-        <div className="promotion_right">
-          <img src={paris} alt="" />
+      <div className="promotion">
+        <div className="container">
+          <h2>Special Promotion</h2>
+          <img src={medicalTeam1} alt="Special Promotion" />
         </div>
       </div>
-      <div classname="trending">
-        <h5 classname="smallred">TRENDY</h5>
-        <h1>Our Trending Tour Packages</h1>
-        <div className="product_cards">
-          <div className="product_card">
-            <div className="imagesss">
-              <img src={product1} alt="" />
-              <img src={logo1} alt="" />
-            </div>
-            <div className="date">
-              {/* <img src={calendar} alt="" />  */}
-              <div className="people">
-                <img src={person} alt="" />
-                <h4>25 people are going</h4>
-              </div>
-              <h1>Switzerland</h1>
-              <h4 className="price"> $1000</h4>
-            </div>
-          </div>
+
+      <div className="trending">
+        <div className="container">
+          <h2>Trending Destinations</h2>
+          <img src={group7} alt="Trending Destination" />
+          <img src={image872} alt="Trending Destination" />
+          <img src={rectangle17} alt="Trending Destination" />
+          <img src={rectangle19305} alt="Trending Destination" />
         </div>
       </div>
-      <gameOver />
+
+      <div className="footer">
+        <div className="container">
+          <img src={ellipse623} alt="Footer" />
+          <img src={userAvatar} alt="Footer" />
+        </div>
+      </div>
     </div>
   );
 }
